1753ea9105edb14cb0c23cc4576a63b39512f567
JellyHead <JellyHeadM@outlook.com>
2022-12-15 21:19:05 -0500

    added type and classname fields on all the buttons on the baseClock component. then installed scss and created the main.scss file so i could start creating and importing scss style sheets. then made a quick style sheet for the base clock that makes it look like a normal claculator. all still functions. want to look into added a back button, need to try and get the modular clock to just grab all the styles it can from the base as well.

--- src/components/BaseClock.js
+++ src/components/BaseClock.js
@@ -36,25 +36,25 @@ function BaseClock() {
                 <div className="calculator-display">
                     {calDisplay !== '' ? calDisplay : '0'}
                 </div>
-                <button onClick={onClick} value='0'>0</button>
-                <button onClick={onClick} value='1'>1</button>
-                <button onClick={onClick} value='2'>2</button>
-                <button onClick={onClick} value='3'>3</button>
-                <button onClick={onClick} value='4'>4</button>
-                <button onClick={onClick} value='5'>5</button>
-                <button onClick={onClick} value='6'>6</button>
-                <button onClick={onClick} value='7'>7</button>
-                <button onClick={onClick} value='8'>8</button>
-                <button onClick={onClick} value='9'>9</button>
-                <button onClick={onClick} value='10'>10</button>
-                <button onClick={onClick} value='+'>+</button>
-                <button onClick={onClick} value='-'>-</button>
-                <button onClick={onClick} value='*'>x</button>
-                <button onClick={onClick} value='/'>/</button>
-                <button onClick={onClick} value='('>(</button>
-                <button onClick={onClick} value=')'>)</button>
-                <button onClick={clearEventHandler} value='clear'>clear</button>
-                <button onClick={equalEventHandler} value='equal'>equal</button>
+                <button onClick={onClick} type='button' className='Calbutton__0' value='0'>0</button>
+                <button onClick={onClick} type='button' className='Calbutton__1' value='1'>1</button>
+                <button onClick={onClick} type='button' className='Calbutton__2' value='2'>2</button>
+                <button onClick={onClick} type='button' className='Calbutton__3' value='3'>3</button>
+                <button onClick={onClick} type='button' className='Calbutton__4' value='4'>4</button>
+                <button onClick={onClick} type='button' className='Calbutton__5' value='5'>5</button>
+                <button onClick={onClick} type='button' className='Calbutton__6' value='6'>6</button>
+                <button onClick={onClick} type='button' className='Calbutton__7' value='7'>7</button>
+                <button onClick={onClick} type='button' className='Calbutton__8' value='8'>8</button>
+                <button onClick={onClick} type='button' className='Calbutton__9' value='9'>9</button>
+                <button onClick={onClick} type='button' className='Calbutton__10' value='+'>+</button>
+                <button onClick={onClick} type='button' className='Calbutton__11' value='-'>-</button>
+                <button onClick={onClick} type='button' className='Calbutton__12' value='*'>x</button>
+                <button onClick={onClick} type='button' className='Calbutton__13' value='/'>/</button>
+                <button onClick={onClick} type='button' className='Calbutton__14' value='('>(</button>
+                <button onClick={onClick} type='button' className='Calbutton__15' value=')'>)</button>
+                <button onClick={onClick} type='button' className='Calbutton__16' value='.'>.</button>
+                <button onClick={clearEventHandler} type='button' className='Calbutton__17' value='clear'>clear</button>
+                <button onClick={equalEventHandler} type='button' className='Calbutton__18' value='equal'>equal</button>
                 
             </div>
         </div>
